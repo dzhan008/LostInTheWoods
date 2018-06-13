6d50a8ca6a6da23b58832038ade5d106722b9dc3
qnguy019 <qnguy019@ucr.edu>
2018-06-13 15:16:02 -0700

    Desert Map Level
    Completed the layout

---
+++ Assets/Scripts/SwitchManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchManager : MonoBehaviour {
+
+    public GameObject equationText;
+    private int numPedestols = 5;
+    private int numAnswer = 3;
+    //public GameObject[] pedestols;
+
+    // Use this for initialization
+    void Start () {
+        string equationText = "";
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
