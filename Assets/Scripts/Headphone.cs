befebc5e96b8e777462db2335b694422139e9622
qnguy019 <qnguy019@ucr.edu>
2018-06-14 03:31:13 -0700

    Headphones and Door Functionality
    The spawn door will open when headphones are picked up.

---
+++ Assets/Scripts/Headphone.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Headphone : MonoBehaviour {
+
+    public GameObject[] appearObj;
+    private void showObjects()
+    {
+        for (int i = 0; i < appearObj.Length; i++)
+        {
+            Debug.Log(i);
+            appearObj[i].GetComponent<Door>().Open();
+        }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" && other.GetComponent<Player>().interacting)
+        {
+            showObjects();
+            Destroy(this.gameObject);
+        }
+    }
+
+
+}
