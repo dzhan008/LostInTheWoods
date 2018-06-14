4801228f131b2d21242443d6f75673b64954e491
dzhan008 <dzhan008@ucr.edu>
2018-06-13 19:00:46 -0700

    Added Water Puzzle/Sound SFX Functionality

---
+++ Assets/Scripts/Puzzles/FirePuzzle/Water.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Pickable")
+        {
+            if(other.GetComponent<PickableObject>().name == "Bucket")
+            {
+                other.GetComponent<Bucket>().ToggleWater(true);
+                SoundManager.Instance.PlaySFX("sfx_water_pick_up");
+            }
+        }
+    }
+}
